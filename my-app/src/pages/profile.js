d7fb7db1cc2c45f2572e73237ad00200dce8b56a
96simben <simoncraft3@hotmail.com>
2020-09-09 14:29:11 +0200

    routes bas klar

---
+++ my-app/src/pages/profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => {
+    return(
+            <div id="profilepage">
+                <h1>the profile page link is working. yippie!</h1>
+                    </div>
+    );
+}
+
+export default Profile;
